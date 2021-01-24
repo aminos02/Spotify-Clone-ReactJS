1013347c51136b30f783ce651a3ae21c5cb5ac17
aminos02 <azzouzamine02@gmail.com>
2021-01-24 23:53:38 +0100

    Add Icons:

--- src/Footer.js
+++ src/Footer.js
@@ -1,9 +1,26 @@
 import React from "react";
 import "./footer.css";
+import PlayCircleOutlineIcon from "@material-ui/icons/PlayCircleOutline";
+import SkipPreviousIcon from "@material-ui/icons/SkipPrevious";
+import SkipNextIcon from "@material-ui/icons/SkipNext";
+import ShuffleIcon from "@material-ui/icons/Shuffle";
+import RepeatIcon from "@material-ui/icons/Repeat";
 function Footer() {
   return (
     <div className="footer">
-      <h1>I m the Footer</h1>
+      <div className="footer_right">
+        <p>left</p>
+      </div>
+      <div className="footer_center">
+        <ShuffleIcon />
+        <SkipPreviousIcon />
+        <PlayCircleOutlineIcon />
+        <SkipNextIcon />
+        <RepeatIcon />
+      </div>
+      <div className="footer_left">
+        <p>right</p>
+      </div>
     </div>
   );
 }
