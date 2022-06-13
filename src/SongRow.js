c91e8c80f440a578e6ef1f8e4072bb8347bfbe14
aminos02 <azzouzamine02@gmail.com>
2022-06-13 17:25:37 +0100

    fix Song Row

--- src/SongRow.js
+++ src/SongRow.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./songrow.css";
 
 function SongRow({ track }) {
-  if(track)
+  if(track?.name && track?.album?.images[0]?.url)
   return (
     <div className="songRow">
       <img className="songRow_album" src={track?.album?.images[0]?.url} alt="" />
